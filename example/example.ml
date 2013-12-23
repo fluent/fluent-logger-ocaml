2cbcf251871158864215743f364aee30d3fdf488
Mitsunori Komatsu <komamitsu@gmail.com>
2013-12-23 12:19:55 +0900

    improve example to test the connection

--- example/example.ml
+++ example/example.ml
@@ -2,13 +2,18 @@ let () =
   Printexc.record_backtrace true;
   let logger = Fluent_logger.create () in
   let open Fluent_logger.Inet in
-  print_endline (string_of_bool (
-    post logger "production" (
-      `FixMap [
-        (of_string "name", of_string "foobar");
-        (of_string "age", uint8_of_int 81);
-        (of_string "pi", of_float 3.14)
-      ]
-    )
-  ));
+  let rec loop n i f = if i < n then (f i; loop n (i + 1) f) in
+  loop 50 0 (fun i ->
+    let result =
+      post logger "production" (
+        `FixMap [
+          (of_string "id", uint8_of_int i);
+          (of_string "name", of_string "foobar");
+          (of_string "age", uint8_of_int 81);
+          (of_string "pi", of_float 3.14)
+        ]
+      ) in
+    if not result then prerr_endline "logger: failed to post";
+    Unix.sleep 1
+  );
   release logger
