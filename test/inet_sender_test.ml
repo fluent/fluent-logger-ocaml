fe8ed02d62ff595e95e6b1dbf5fb4d98a208ff1f
Mitsunori Komatsu <komamitsu@gmail.com>
2013-12-23 11:38:41 +0900

    improve inet_sender_test

--- test/inet_sender_test.ml
+++ test/inet_sender_test.ml
@@ -1,60 +1,65 @@
 open OUnit
+module U = Unix
 
 let setup_test_server () =
-  let socket = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-  Unix.setsockopt socket Unix.TCP_NODELAY true;
-  Unix.set_close_on_exec socket;
-  Unix.bind socket (Unix.ADDR_INET(Unix.inet_addr_any, 0));
-  Unix.listen socket 50;
-  let ev_ch = Event.new_channel () in
+  let socket = U.socket U.PF_INET U.SOCK_STREAM 0 in
+  U.setsockopt socket U.TCP_NODELAY true;
+  U.set_close_on_exec socket;
+  U.bind socket (U.ADDR_INET(U.inet_addr_any, 0));
+  U.listen socket 50;
+  let recv_buf = Buffer.create 512 in
+  let current_accept_socket = ref None in
   ignore (
     Thread.create (fun _ ->
-      let (client_socket, sockaddr) = Unix.accept socket in
-      ignore (Event.sync (Event.send ev_ch client_socket));
-      let in_ch = Unix.in_channel_of_descr client_socket in
+      let (cs, sockaddr) = U.accept socket in
+      current_accept_socket := Some cs; 
+      let in_ch = U.in_channel_of_descr cs in
       let rec _loop () =
         let buf = String.create 1024 in
-        let len = input in_ch buf 0 1024 in
-        if len = 0 then (
-          close_in in_ch
-        )
+        let len = input in_ch buf 0 (String.length buf) in
+        if len = 0 then close_in in_ch
         else (
-          print_endline (String.sub buf 0 len);
+          Buffer.add_string recv_buf (String.sub buf 0 len);
           _loop ()
         )
         in
       _loop ()
     ) ()
   );
-  match Unix.getsockname socket with
-  | Unix.ADDR_INET (_, port) ->
-      (socket, port, (fun () -> Event.sync (Event.receive ev_ch)))
+  match U.getsockname socket with
+  | U.ADDR_INET (_, port) ->
+      (socket, port, 
+        (fun () -> 
+          match !current_accept_socket with
+          Some cs -> cs | None -> failwith "no accept_socket"),
+        (fun () -> Buffer.contents recv_buf))
   | _ -> failwith "unexpected socket domain"
 
 let test_inet_sender_write () =
   (* setup *)
-  let (socket, port, recv_client_socket) = setup_test_server () in
+  let (socket, port, get_accept_socket, get_recv_data) = setup_test_server () in
   let sender = Inet_sender.create ~port:port () in
   (* first msg *)
-  let msg = "helloworld" in
-  let msg_len = String.length msg in
-  assert_equal (Some msg_len) (Inet_sender.write sender msg 0 msg_len);
+  let msg1 = "helloworld" in
+  let msg_len = String.length msg1 in
+  assert_equal (Some msg_len) (Inet_sender.write sender msg1 0 msg_len);
   (* second msg *)
-  let msg = "abracadabra" in
-  let msg_len = String.length msg in
-  assert_equal (Some msg_len) (Inet_sender.write sender msg 0 msg_len);
+  let msg2 = "abracadabra" in
+  let msg_len = String.length msg2 in
+  assert_equal (Some msg_len) (Inet_sender.write sender msg2 0 msg_len);
   (* close server and then third msg *)
-  let client_socket = recv_client_socket () in
-  Unix.close client_socket;
-  Unix.close socket;
+  U.sleep 1;
+  let accept_socket = get_accept_socket () in
+  U.close accept_socket;
+  U.close socket;
   let msg = "foobar" in
   let msg_len = String.length msg in
-  assert_equal None (Inet_sender.write sender msg 0 msg_len)
+  assert_equal None (Inet_sender.write sender msg 0 msg_len);
+  assert_equal (msg1 ^ msg2) (get_recv_data ())
 
 let suite =
   "Fluent_logger tests" >:::
-    ["test_inet_sender_write" >:: test_inet_sender_write
-    ]
+    ["test_inet_sender_write"         >:: test_inet_sender_write]
 
 let () =
   Printexc.record_backtrace true;
